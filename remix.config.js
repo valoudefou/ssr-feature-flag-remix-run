023cd99a4683b15f89abbfd9df6b9878b0e92b6f
valoudefou <valerian.karsenty@gmail.com>
2025-07-11 14:27:46 +0100

    Update package.json

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from '@remix-run/dev';
-import vercel from '@remix-run/vercel';
 
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 export default defineConfig({
   serverBuildTarget: "vercel",
-  server: vercel(),
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
